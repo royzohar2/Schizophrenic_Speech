3ebc70936f9a1a60f48f39131777cf873c73a4cc
seanhyper <123296553+seanhyper@users.noreply.github.com>
2024-08-28 01:06:47 +0300

    add ground truth feat

--- feature_extraction/ground_truth_distance.py
+++ feature_extraction/ground_truth_distance.py
@@ -1,27 +1,28 @@
-from data_loader import DataLoader
 import pandas as pd
-from test_study.coherence.features import TextFeatureExtractor
 from torch.nn import CosineSimilarity
 import numpy as np
 
-CLEAN_DATA_PATH = "Data/clean_data.csv"
-GROUND_TRUTH_DOCX_PATH = "Data/תיאור התמונות.docx"
+from feature_extraction.morphological import MorphologicalFeatureExtractor
+from infra.data_loader import DataLoader
+
+CLEAN_DATA_PATH = "data/clean_data.csv"
+GROUND_TRUTH_DOCX_PATH = "data/תיאור התמונות.docx"
+
 
 class GroundTruthDistance:
     def __init__(self):
         self.data = pd.read_csv(CLEAN_DATA_PATH)
         self.ground_truth_docx_path = GROUND_TRUTH_DOCX_PATH
         self.data_loader = DataLoader('')
-        self.feat_extractor = TextFeatureExtractor()
+        self.feat_extractor = MorphologicalFeatureExtractor()
         self.cosine_similarity = CosineSimilarity(dim = 0, eps = 1e-6)
-
 
     def create_ground_truth_df(self):
         content = self.data_loader._read_docx(self.ground_truth_docx_path)
         data = self.data_loader._extract_data(content)
         ground_truth_df = pd.DataFrame([data])
         return ground_truth_df
-    
+
     def create_embeddings_df(self, original_df):
         embeddings_df = pd.DataFrame()
 
@@ -29,7 +30,7 @@ class GroundTruthDistance:
             embeddings_df[col] = original_df[col].apply(self.feat_extractor.get_sentence_embedding)
 
         return embeddings_df
-    
+
     def calculate_ground_truth_distances(self):
         ground_truth_df = self.create_ground_truth_df()
         ground_truth_embeddings = self.create_embeddings_df(ground_truth_df)
@@ -37,18 +38,18 @@ class GroundTruthDistance:
         relevat_columns = [col for col in self.data.columns if 'תמונה' in col]
         data_embeddings = self.create_embeddings_df(self.data[relevat_columns])
 
-        distances_df = pd.DataFrame(columns=data_embeddings.columns)
+        distances_df = pd.DataFrame(columns = data_embeddings.columns)
         distances_df['label'] = self.data['label']
 
         for col in distances_df:
             if col != 'label':
                 ground_truth_embedding = np.reshape(ground_truth_embeddings[col].iloc[0], [-1, 1])
-                distances_df[col] = data_embeddings[col].apply(lambda x: self.cosine_similarity(ground_truth_embedding, np.reshape(x, [-1, 1])).item())
+                distances_df[col] = data_embeddings[col].apply(
+                    lambda x: self.cosine_similarity(ground_truth_embedding, np.reshape(x, [-1, 1])).item())
         return distances_df
 
 
 if __name__ == '__main__':
-
     distance_calculator = GroundTruthDistance()
     distances = distance_calculator.calculate_ground_truth_distances()
-    distances.to_csv('Data/ground_truth_distances.csv', index=False)
+    distances.to_csv('Data/ground_truth_distances.csv', index = False)
